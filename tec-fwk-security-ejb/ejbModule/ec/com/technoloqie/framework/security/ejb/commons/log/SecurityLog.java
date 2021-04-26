e8143de5d3a8318d44d2f579184e6532070f4b1e
thc <dijavaji@gmail.com>
2021-04-26 02:05:04 -0500

    cambio logger a singleton

--- tec-fwk-security-ejb/ejbModule/ec/com/technoloqie/framework/security/ejb/commons/log/SecurityLog.java
+++ tec-fwk-security-ejb/ejbModule/ec/com/technoloqie/framework/security/ejb/commons/log/SecurityLog.java
@@ -2,12 +2,34 @@ package ec.com.technoloqie.framework.security.ejb.commons.log;
 
 import org.apache.log4j.Logger;
 
+/**
+ * Clase para el registro de los mensajes de una determinada transaccion en la ejecucion del programa
+ * @author technoloqie
+ *
+ */
 public final class SecurityLog {
 	
 	static final Logger logger = Logger.getLogger(SecurityLog.class);
-	private static final SecurityLog INSTANCIA = new SecurityLog();
+	private static SecurityLog instance = new SecurityLog();
+	private static String valueClazz;
 	
-	public static void info(String parameter){
+	public static SecurityLog getLog() {
+		if (instance == null) {
+		    instance = new SecurityLog();
+		}
+		return instance;
+	}
+	 
+	@SuppressWarnings("rawtypes")
+	public static SecurityLog getLog(Class clazz) {
+		if (instance == null) {
+		    instance = new SecurityLog();
+		}
+		valueClazz = clazz.getName();
+		return instance;
+    } 
+	
+	public void info(String parameter){
 		logger.info(parameter);
 	}
 	
@@ -19,20 +41,17 @@ public final class SecurityLog {
 		logger.warn("This is warn : " + parameter);
 		}
 	
-	public static void error(String parameter){
+	public void error(String parameter){
 		logger.error(parameter);
 	}
 	
 	public void fatal(String parameter){
 		logger.fatal("This is fatal : " + parameter);
 	}
-	
-	public static void getLog(){
-		
-	}
 
-	public static void error(String string, Exception e) {
-		error(string + e.getMessage());
+	public void error(String string, Exception e) {
+		valueClazz = valueClazz != null ? "[" + valueClazz + "] ": ""; 
+		error(valueClazz + string + e.getMessage());
 	}
 
 }
